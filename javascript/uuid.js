b0a09c3127854eb3384c6b67aed8c7d28dcdb79f
Tristan Cavelier <t.cavelier@free.fr>
2013-06-22 10:41:55 +0200

    Uuid generator added

---
+++ javascript/uuid.js
@@ -0,0 +1,19 @@
+
+// keywords: js, javascript, uuid generator
+
+function uuid() {
+  var S4 = function () {
+    var i, string = Math.floor(
+      Math.random() * 0x10000 /* 65536 */
+    ).toString(16);
+    for (i = string.length; i < 4; i += 1) {
+      string = '0'+string;
+    }
+    return string;
+  };
+  return S4() + S4() + "-" +
+    S4() + "-" +
+    S4() + "-" +
+    S4() + "-" +
+    S4() + S4() + S4();
+};
