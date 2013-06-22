864b236332d4fafe416af46bfde81e073bb4d8d6
Tristan Cavelier <t.cavelier@free.fr>
2013-06-22 10:46:51 +0200

    Node js utilities.inherits function like added + example

---
+++ javascript/inherits.js
@@ -0,0 +1,47 @@
+
+// keywords: javascript, inherits, class, prototype
+
+/**
+ * Inherits the prototype methods from one constructor into another. The
+ * prototype of `constructor` will be set to a new object created from
+ * `superConstructor`.
+ */
+function inherits(constructor, superConstructor) {
+  constructor.super_ = superConstructor;
+  superConstructor.prototype = constructor.prototype;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// example
+
+function A() {
+  this.a = 2;
+}
+A.prototype = {
+  "b": function () {
+    console.log(this.a);
+  }
+};
+
+var a = new A();
+console.log(a.a);
+a.b();
+
+
+
+function C() {
+  A.call(this);
+  this.c = 3;
+}
+
+inherits(A, C);
+
+C.prototype.d = function () {
+  console.log(this.a);
+  this.b();
+}
+
+var c = new C();
+console.log(c.a);
+console.log(c.c);
+c.d();
