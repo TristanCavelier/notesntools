a2a507f30fd366d6d73eaee4f4cd68fb47c21554
Tristan Cavelier <t.cavelier@free.fr>
2013-08-05 15:07:29 +0200

    isDate.js bug isDate(new Date('aeoua')) -> fixed

--- javascript/isDate.js
+++ javascript/isDate.js
@@ -2,16 +2,13 @@
 // keywords: js, javascript, is date tester
 
 /**
- * Test if the a value can be converted to Date
+ * Test if the a value is a date
  *
  * @method <method_name>
  * @param  {String,Number,Date} date The date to test
  * @return {Boolean} true if success, else false
  */
 function isDate(date) {
-  if (date instanceof Date) {
-    return true;
-  }
   if (!isNaN((new Date(date)).getTime())) {
     return true;
   }
@@ -19,5 +16,5 @@ function isDate(date) {
 }
 
 console.log(isDate('2013'));
-console.log(isDate(new Date()));
+console.log(isDate(new Date('2013')));
 console.log(isDate(1234));
