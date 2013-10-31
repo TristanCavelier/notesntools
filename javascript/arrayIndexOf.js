827f88d215c6c66d0126fc4dca440559df86819f
Tristan Cavelier <t.cavelier@free.fr>
2013-10-31 10:22:24 +0100

    arrayIndexOf.js added

---
+++ javascript/arrayIndexOf.js
@@ -0,0 +1,27 @@
+// keywords: js, javascript, benchmark, indexOf, find, array, faster
+/*jslint indent: 2, maxlen: 80 */
+"use strict";
+//////////////////////////////////////////////////////////////////////
+
+/**
+ *     arrayIndexOf(array, value, from): number
+ *
+ * Like 'Array.indexOf()' in javascript, but 5-7 times faster.
+ *
+ * @param  {Array} array The array to browse
+ * @param  {Any} value The value to find
+ * @param  {Number} from The index where to begin
+ * @return {Number} -1 if not found, else the index of the first value found
+ */
+function arrayIndexOf(array, value, from) {
+  var index, length = array.length;
+  for (index = from || 0; index < length; index += 1) {
+    if (array[index] === value) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+//////////////////////////////////////////////////////////////////////
+exports.arrayIndexOf = arrayIndexOf;
